2e22746fac68d03d70180693570b7b1b00e8720e
A Aayush <aaayush879@gmail.com>
2021-05-07 15:22:27 +0530

    firstt

--- src/mom.js
+++ src/mom.js
@@ -4,16 +4,16 @@ import LogoImage from './new.jpg';
 import './mom.css'
 
 const mom=()=>{
-    return<div style={{ width:"100%", height:"100%" , textAlign:"center" ,backgroundImage: `url(${LogoImage})` , backgroundRepeat:"no-repeat", backgroundSize:"Cover", backgroundPosition: "center"}}>
+    return<div style={{ width:"100%", height:"100vh" , textAlign:"center" ,backgroundImage: `url(${LogoImage})` , backgroundRepeat:"no-repeat", backgroundSize:"Cover", backgroundPosition: "center"}}>
         <div className="sap text-success text-center" >
             <h1>Happy Mothers Day Mummy</h1>
         </div>
         
-        <div className="are" style={{ alignItems:"center", alignContent:"justify" , marginBottom:"20%" , marginLeft:"30%" , borderRadius:"20%"}}>
+        <div className="are" style={{ alignItems:"center", alignContent:"justify" , marginBottom:"25%" , marginLeft:"30%" , borderRadius:"20%"}}>
         <Present/>
         </div>
 
-        <div className="text-center text-white " style={{widht:"100%" , height:"10%" , backgroundColor:"black"}}>
+        <div className="text-center text-white " style={{widht:"100%" , height:"8%" , backgroundColor:"black" , marginBottom:"10px"}}>
             <h3>Made by me</h3>
 
         </div>
